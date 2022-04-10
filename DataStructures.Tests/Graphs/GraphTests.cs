a02d08a631851b578523eec2e34c9dc1305b2ffa
davatata <davatata@gmail.com>
2022-04-10 14:39:12 -0700

    [other] added Graph, DFS, BFS, tests

---
+++ DataStructures.Tests/Graphs/GraphTests.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Graphs.Tests
+{
+    [TestClass()]
+    public class GraphTests
+    {
+        Graph graphU;
+
+        [ClassInitialize]
+        public static void TestFixtureSetup(TestContext context)
+        {
+            // Executes once for the test class. (Optional)
+        }
+        [TestInitialize]
+        public void Setup()
+        {
+            // Runs before each test. (Optional)
+            graphU = new Graph();
+
+            graphU.nodeLookup.Add(1, new Graph.Node(1));
+            graphU.nodeLookup.Add(2, new Graph.Node(2));
+            graphU.nodeLookup.Add(3, new Graph.Node(3));
+            graphU.nodeLookup.Add(4, new Graph.Node(4));
+            graphU.nodeLookup.Add(5, new Graph.Node(5));
+            graphU.nodeLookup.Add(6, new Graph.Node(6));
+            graphU.nodeLookup.Add(7, new Graph.Node(7));
+            graphU.nodeLookup.Add(8, new Graph.Node(8));
+
+            graphU.AddEdgeUndirect(1, 2);
+            graphU.AddEdgeUndirect(1, 3);
+            graphU.AddEdgeUndirect(2, 4);
+            graphU.AddEdgeUndirect(4, 5);
+            graphU.AddEdgeUndirect(4, 7);
+            graphU.AddEdgeUndirect(5, 7);
+            graphU.AddEdgeUndirect(5, 6);
+            graphU.AddEdgeUndirect(7, 6);
+            graphU.AddEdgeUndirect(6, 8);
+            graphU.AddEdgeUndirect(7, 8);
+        }
+
+        [TestMethod()]
+        public void AddEdgeUndirect_ShouldAdd2Edges()
+        {
+            // Arrange
+            //var graphU = new Graph();
+
+            // Act
+            graphU.AddEdgeUndirect(1, 2);
+
+            // Assert
+            var v1Edges = graphU.GetNode(1).Adjacent;
+            var v2Edges = graphU.GetNode(2).Adjacent;
+
+            Assert.AreEqual(v1Edges.Count, v2Edges.Count);
+        }
+
+        [TestMethod()]
+        public void AddEdgeDirect_ShouldAdd1Edges()
+        {
+            // Arrange
+            //var graphU = new Graph();
+
+            // Act
+            graphU.AddEdgeDirect(1, 2);
+
+            // Assert
+            var v1Edges = graphU.GetNode(1).Adjacent;
+            var v2Edges = graphU.GetNode(2).Adjacent;
+
+            Assert.AreNotEqual(v1Edges.Count, v2Edges.Count);
+        }
+
+        #region DataRows1
+        [DataTestMethod()]
+        [DataRow(1, 2)]
+        [DataRow(2, 1)]
+
+        [DataRow(1, 3)]
+        [DataRow(3, 1)]
+
+        [DataRow(2, 4)]
+        [DataRow(4, 2)]
+
+        [DataRow(4, 5)]
+        [DataRow(5, 4)]
+
+        [DataRow(4, 7)]
+        [DataRow(7, 4)]
+
+        [DataRow(5, 7)]
+        [DataRow(7, 5)]
+
+        [DataRow(5, 6)]
+        [DataRow(6, 5)]
+
+        [DataRow(6, 7)]
+        [DataRow(7, 6)]
+
+        [DataRow(8, 7)]
+        [DataRow(7, 8)]
+
+        [DataRow(6, 8)]
+        [DataRow(8, 6)]
+        #endregion
+        public void HasPathDFS_ShouldHavePathFrom_x_To_y(int x, int y)
+        {
+            Assert.IsTrue(graphU.HasPathDFS(x, y));
+        }
+
+        #region DataRows1
+        [DataTestMethod()]
+        [DataRow(1, 2)]
+        [DataRow(2, 1)]
+
+        [DataRow(1, 3)]
+        [DataRow(3, 1)]
+
+        [DataRow(2, 4)]
+        [DataRow(4, 2)]
+
+        [DataRow(4, 5)]
+        [DataRow(5, 4)]
+
+        [DataRow(4, 7)]
+        [DataRow(7, 4)]
+
+        [DataRow(5, 7)]
+        [DataRow(7, 5)]
+
+        [DataRow(5, 6)]
+        [DataRow(6, 5)]
+
+        [DataRow(6, 7)]
+        [DataRow(7, 6)]
+
+        [DataRow(8, 7)]
+        [DataRow(7, 8)]
+
+        [DataRow(6, 8)]
+        [DataRow(8, 6)]
+        #endregion
+        public void HasPathBFS_ShouldHavePathFrom_x_To_y(int x, int y)
+        {
+            Assert.IsTrue(graphU.HasPathBFS(x, y));
+        }
+    }
+}
---
+++ InterviewPreparation/Graphs/Graph.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.Graphs
+{
+    public class Graph
+    {
+        public Dictionary<int, Node> nodeLookup = new Dictionary<int, Node>();
+
+        public class Node
+        {
+            public int Id;
+            public LinkedList<Node> Adjacent = new LinkedList<Node>();
+            public Node(int id)
+            {
+                Id = id;
+            }
+        }
+
+        public Node GetNode(int id)
+        {
+            return nodeLookup[id];
+        }
+
+        public void AddEdgeDirect(int source, int destination)
+        {
+            Node s = GetNode(source);
+            Node d = GetNode(destination);
+            s.Adjacent.AddLast(d);
+        }
+
+        public void AddEdgeUndirect(int source, int destination)
+        {
+            Node s = GetNode(source);
+            Node d = GetNode(destination);
+            s.Adjacent.AddLast(d);
+            d.Adjacent.AddLast(s);
+        }
+
+        public bool HasPathDFS(int source, int destination)
+        {
+            Node s = GetNode(source);
+            Node d = GetNode(destination);
+            var visited = new HashSet<int>();
+            return HasPathDFS(s, d, visited);
+        }
+
+        private bool HasPathDFS(Node source, Node destination, HashSet<int> visited)
+        {
+            if (visited.Contains(source.Id))
+                return false;
+
+            visited.Add(source.Id);
+            if (source == destination)
+                return true;
+
+            foreach (Node child in source.Adjacent)
+            {
+                if (HasPathDFS(child, destination, visited))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool HasPathBFS(int source, int destination)
+        {
+            return HasPathBFS(GetNode(source), GetNode(destination));
+        }
+
+        private bool HasPathBFS(Node source, Node destination)
+        {
+            var nextToVisit = new Queue<Node>();
+            var visited = new HashSet<int>();
+            nextToVisit.Enqueue(source);
+
+            while (nextToVisit.Count > 0)
+            {
+                Node node = nextToVisit.Dequeue();
+                if (node == destination)
+                    return true;
+
+                if (visited.Contains(node.Id))
+                {
+                    continue;
+                }
+                visited.Add(node.Id);
+
+                foreach (var child in node.Adjacent)
+                {
+                    nextToVisit.Enqueue(child);
+                }
+            }
+
+            return false;
+        }
+    }
+}
