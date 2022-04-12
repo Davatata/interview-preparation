03c556c28482df204298d39c79e7d131fcbe63d4
davatata <davatata@gmail.com>
2022-04-11 22:17:22 -0700

    Tested object reference passing

--- InterviewPreparation/Program.cs
+++ InterviewPreparation/Program.cs
@@ -1,17 +1,29 @@
 ﻿using DataStructures.LinkedLists;
 using System;
+using System.Collections.Generic;
 
 namespace InterviewPreparation
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            var list = new LinkedList<int>
-            {
-                Head = new Node<int>(4)
-            };
+            var node1 = new Node<int>(1);
+            var node2 = new Node<int>(2);
 
+            var list = new List<Node<int>>{ node1, node2 };
+
+            for(var i = 3; i < 10; i++)
+            {
+                AddNode(i, list);
+            }
+            Console.WriteLine(list);
+        }
+
+        public static void AddNode(int x, List<Node<int>> list)
+        {
+            list.Add(new Node<int>(x));
         }
     }
 }
