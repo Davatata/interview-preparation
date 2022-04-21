2b1ba5d0b6de045b2fda2587396453aea5860b40
davatata <davatata@gmail.com>
2022-04-20 21:30:47 -0700

    Added two pointer method and test

---
+++ DataStructures.Tests/Two-Pointers/TwoPointersTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.Two_Pointers.Tests
+{
+    [TestClass()]
+    public class TwoPointersTests
+    {
+        [TestMethod()]
+        [DataRow("abcabcbb", 3)]
+        [DataRow("au", 2)]
+        [DataRow("dvdf", 3)]
+        [DataRow("aab", 2)]
+        public void LengthOfLongestSubstring(string str, int expected)
+        {
+            var result = TwoPointers.LengthOfLongestSubstring(str);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ InterviewPreparation/Two-Pointers/TwoPointers.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.Two_Pointers
+{
+    public class TwoPointers
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length < 2)
+            {
+                return s.Length;
+            }
+
+            var left = 0;
+            var right = 0;
+            var longestSub = 0;
+
+            var seenLetters = new HashSet<char>();
+
+            while (right < s.Length)
+            {
+                if(!seenLetters.Contains(s[right]))
+                {
+                    seenLetters.Add(s[right]);
+
+                    if (seenLetters.Count > longestSub)
+                    {
+                        longestSub = seenLetters.Count;
+                    }
+                } else {
+                    //if (seenLetters.Count > longestSub)
+                    //{
+                    //    longestSub = seenLetters.Count;
+                    //}
+
+                    seenLetters.Clear();
+
+                    left = right;
+                    seenLetters.Add(s[right]);
+                }
+
+                right++;
+            }
+
+            return longestSub;
+        }
+    }
+}
